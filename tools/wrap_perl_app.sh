f7baa9e2a17b64ffbe68dc21265a35dd418f5066
Robert Olson <olson@mcs.anl.gov>
2012-06-28 13:49:17 -0500

    Updated release engineering stuff

---
+++ tools/wrap_perl_app.sh
@@ -0,0 +1,25 @@
+#
+# Wrap a perl script for execution in a mac .app wrapper
+#
+
+if [ $# -ne 2 ] ; then
+    echo "Usage: $0 script-name dest" 1>&2 
+    exit 1
+fi
+
+script=`basename $1`
+dst=$2
+
+cat > $dst <<EOF
+#!/bin/bash
+dir=\`dirname "\$0"\`
+dir=\`cd "\$dir/../.."; pwd\`
+
+export KB_TOP="\$dir/deployment"
+export KB_RUNTIME="\$dir/runtime"
+export PATH="\$KB_RUNTIME/bin:\$KB_TOP/bin:\$PATH"
+export PERL5LIB="\$KB_TOP/lib"
+"\$KB_RUNTIME/bin/perl" "\$KB_TOP/plbin/$script" "\$@"
+EOF
+
+chmod +x $dst
