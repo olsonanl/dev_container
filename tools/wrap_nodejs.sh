cb4cf54c087bbaf051eb8c1238df4576c4a44666
Robert Olson <olson@mcs.anl.gov>
2018-03-22 15:06:47 -0500

    Add nodejs support.

---
+++ tools/wrap_nodejs.sh
@@ -0,0 +1,41 @@
+#
+# Wrap a nodejs script for execution in the development runtime environment.
+#
+
+if [ $# -ne 2 ] ; then
+    echo "Usage: $0 source dest" 1>&2 
+    exit 1
+fi
+
+src=$1
+dst=$2
+
+if [ "$KB_OVERRIDE_TOP" != "" ] ; then
+    top=$KB_OVERRIDE_TOP
+else
+    top=$KB_TOP
+fi
+
+if [ "$KB_OVERRIDE_RUNTIME" != "" ] ; then
+    runtime=$KB_OVERRIDE_RUNTIME
+else
+    runtime=$KB_RUNTIME
+fi
+
+cat > $dst <<EOF1
+#!/bin/sh
+export KB_TOP=$top
+export KB_RUNTIME=$runtime
+export PATH=$runtime/bin:$top/bin:\$PATH
+EOF1
+for var in $WRAP_VARIABLES ; do
+	val=${!var}
+	if [ "$val" != "" ] ; then
+		echo "export $var='$val'" >> $dst
+	fi
+done
+cat >> $dst <<EOF
+$NODE $src "\$@"
+EOF
+
+chmod +x $dst
